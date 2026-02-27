21e4026d089648a18205227f60d32e142b52bbbc
Ewan Croft <git@ewancroft.uk>
2026-02-27 07:59:33 +0000

    feat(web): define __WEB_VERSION__ and __CLI_VERSION__ at build time and show in sticky header

--- packages/malachite-web/src/app.d.ts
+++ packages/malachite-web/src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+declare const __WEB_VERSION__: string;
+declare const __CLI_VERSION__: string;
+
 declare global {
 	namespace App {
 		// interface Error {}
