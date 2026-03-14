9e30f7a37074850b12ba432b1704f484ad9870c5
Ewan Croft <git@ewancroft.uk>
2026-03-14 15:39:26 +0000

    chore: update pnpmDeps hash for noise-avatar lockfile change

--- packages/pds-landing/default.nix
+++ packages/pds-landing/default.nix
@@ -17,7 +17,7 @@ stdenv.mkDerivation (finalAttrs: {
     inherit (finalAttrs) pname version src;
     fetcherVersion = 2;
     # Run `nix build` and replace with the hash from the error output.
-    hash = "sha256-AWDUq0ysY/2XAEFajccDvWzfTkKnlov2UA4qa5SoiRU=";
+    hash = "sha256-V4gAO78dCsBE829NM3WfFKOYYSrs4IvbuqpHiL7b8+0=";
   };
 
   nativeBuildInputs = [
