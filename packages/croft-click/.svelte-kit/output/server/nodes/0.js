7007506a6af8f29e7856b5d2b426c9844c104bcc
Ewan Croft <git@ewancroft.uk>
2026-04-28 19:44:41 +0100

    chore(croft-click): add .gitignore, remove .svelte-kit from tracking

--- packages/croft-click/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DKLJO5ra.js","_app/immutable/chunks/DMY-DcD8.js","_app/immutable/chunks/vLEuCWTH.js","_app/immutable/chunks/lydTNRRv.js","_app/immutable/chunks/BiIgwehZ.js"];
-export const stylesheets = ["_app/immutable/assets/0.Tf9O3pOo.css"];
-export const fonts = [];
