f24519195f7103d26751b07bb0f3e91493d09ef9
Ewan Croft <git@ewancroft.uk>
2026-05-18 23:03:22 +0100

    feat(landing-ui): add SearchInput component

--- packages/landing-ui/src/lib/index.ts
+++ packages/landing-ui/src/lib/index.ts
@@ -1,2 +1,3 @@
 export { default as LandingLayout } from './LandingLayout.svelte';
 export { default as LandingPage } from './LandingPage.svelte';
+export { default as SearchInput } from './SearchInput.svelte';
