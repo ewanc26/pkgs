93d8c11ae1aa107c1d4cd069b3972976ddb66869
Ewan Croft <me@ewancroft.uk>
2026-06-13 19:58:51 +0100

    Export documentationTemplate from built-in templates index

--- packages/og/src/templates/index.ts
+++ packages/og/src/templates/index.ts
@@ -5,16 +5,19 @@
 export { blogTemplate } from './blog.js'
 export { profileTemplate } from './profile.js'
 export { defaultTemplate } from './default.js'
+export { documentationTemplate } from './Documentation.js'
 
 import { blogTemplate } from './blog.js'
 import { profileTemplate } from './profile.js'
 import { defaultTemplate } from './default.js'
+import { documentationTemplate } from './Documentation.js'
 import type { OgTemplate } from '../types.js'
 
 export const templates = {
 	blog: blogTemplate,
 	profile: profileTemplate,
 	default: defaultTemplate,
+	documentation: documentationTemplate,
 } as const
 
 export type TemplateName = keyof typeof templates
